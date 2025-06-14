5ffeb816297ea56ea17093098fc90ee1f7e08e32
Liamber1234 <liam.berguig@gmail.com>
2025-06-14 17:26:34 +0300

    todo quest struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod todoListItem;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/todoListItem.rs
@@ -0,0 +1,33 @@
+use std::fmt;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TodoListQuest {
+    description: String,
+    finished: bool
+}
+
+impl fmt::Display for TodoListQuest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.description)
+    }
+}
+
+impl TodoListQuest {
+    pub fn new(description: String) -> Self
+    {
+        TodoListQuest {description, finished: false}
+    }
+    
+    
+    
+    pub fn finish_quest(&mut self) 
+    {
+        self.finished = true;
+    } 
+    
+    pub fn is_finished(&self) -> bool
+    {
+        self.finished
+    }
+}
